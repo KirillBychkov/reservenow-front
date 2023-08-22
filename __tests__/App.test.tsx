517de0bf6694d341de4748f7fca41de6bd47bd98
Nazofer <nvovk.2004@gmail.com>
2023-08-22 17:23:44 +0300

    feat: setup testing environment

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from '../src/App';
+
+describe('App', () => {
+  it('should work as expected', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText('Hello World!')).toBeInTheDocument();
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,26 @@
+export default {
+  clearMocks: true,
+
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-transformer.js',
+    '^.+\\.(css|scss|sass|less)$': '<rootDir>/__mocks__/style-transformer.js',
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'ecmascript',
+            jsx: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
+}

6598cf80754e96401f9fac764670fc2db67f1d63
illia.kyrychenko <illia.kyrychenko@ficuslife.com>
2023-11-09 08:39:10 +0000

    Faq page realisation

---
+++ src/constants/faq.ts
@@ -0,0 +1,27 @@
+type FAQItem = {
+  localizationQuestionKey: string;
+  localizationAnswerKey: string;
+};
+
+export const questions: FAQItem[] = [
+  {
+    localizationQuestionKey: "faq.questions.first.question",
+    localizationAnswerKey: "faq.questions.first.answer",
+  },
+  {
+    localizationQuestionKey: "faq.questions.second.question",
+    localizationAnswerKey: "faq.questions.second.answer",
+  },
+  {
+    localizationQuestionKey: "faq.questions.third.question",
+    localizationAnswerKey: "faq.questions.third.answer",
+  },
+  {
+    localizationQuestionKey: "faq.questions.fourth.question",
+    localizationAnswerKey: "faq.questions.fourth.answer",
+  },
+  {
+    localizationQuestionKey: "faq.questions.fifth.question",
+    localizationAnswerKey: "faq.questions.fifth.answer",
+  },
+];

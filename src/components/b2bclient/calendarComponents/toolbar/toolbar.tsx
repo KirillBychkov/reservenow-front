d080b5a8e2cd1e4b808147643392221b94f3d0d3
Illia Kyrychenko <i.kirichenko160804@gmail.com>
2024-01-17 17:12:29 +0200

    Schedule styling. Custom Calendar component to manage custom components of schedule

--- src/components/b2bclient/calendarComponents/toolbar/toolbar.tsx
+++ src/components/b2bclient/calendarComponents/toolbar/toolbar.tsx
@@ -1,18 +1,39 @@
-import Button from "@/components/UI/buttons/button";
-import CustomDropdown from "@/components/UI/dropdown/customDropdown";
-import Flex from "@/components/UI/layout/flex";
-import { ChevronLeft, ChevronRight } from "@blueprintjs/icons";
-import { DropdownChangeEvent } from "primereact/dropdown";
-import { useMemo } from "react";
-import { ToolbarProps } from "react-big-calendar";
-import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
-import styles from './toolbar.module.scss'
+import Button from '@/components/UI/buttons/button';
+import CustomDropdown from '@/components/UI/dropdown/customDropdown';
+import Flex from '@/components/UI/layout/flex';
+import { ChevronLeft, ChevronRight } from '@blueprintjs/icons';
+import { DropdownChangeEvent } from 'primereact/dropdown';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import styles from './toolbar.module.scss';
+import { ToolbarProps } from 'react-big-calendar';
+import { Events, ObjectUtils, OrganizationUtils } from '@/types/schedule';
 
-export const Toolbar = ({ view, onView, onNavigate, views, label }: ToolbarProps) => {
+type Props = {
+  orgUtils: OrganizationUtils;
+  objUtils: ObjectUtils;
+  currentEvent: Events;
+} & ToolbarProps;
+
+const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const Toolbar = ({
+  view,
+  onView,
+  onNavigate,
+  views,
+  label,
+  objUtils,
+  orgUtils,
+  currentEvent,
+}: Props) => {
   const viewsArray = views as Array<string>;
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const { onObjChange, objOptions, objectName } = objUtils;
+  const { orgName, onOrgChange, orgOptions } = orgUtils;
+
   const handleNext = () => {
     onNavigate('NEXT');
   };
@@ -46,7 +67,32 @@ export const Toolbar = ({ view, onView, onNavigate, views, label }: ToolbarProps
             <ChevronRight size={20} color='#7961DB' />
           </div>
         </Flex>
-        <h4 className='heading heading-4 heading-primary'>{label}</h4>
+        <h4 className='heading heading-4 heading-primary'>
+          {capitalize(label)}
+        </h4>
+
+        {currentEvent === Events.Organizations && (
+          <Flex options={{ gap: 1 }}>
+            <CustomDropdown
+              placeholder={
+                orgName || t('schedule.form.objectSection.chooseObject')
+              }
+              onChange={onOrgChange}
+              className={styles.dropdown}
+              value={orgName}
+              options={orgOptions}
+            />
+            <CustomDropdown
+              placeholder={
+                objectName ||
+                t('schedule.form.objectSection.chooseOrganization')
+              }
+              className={styles.dropdown}
+              options={objOptions}
+              onChange={onObjChange}
+            />
+          </Flex>
+        )}
       </Flex>
 
       <Flex options={{ gap: 1, align: 'center' }}>
@@ -55,8 +101,9 @@ export const Toolbar = ({ view, onView, onNavigate, views, label }: ToolbarProps
           placeholder={view}
           value={view}
           options={ViewOptions}
+          className={styles.viewDropdown}
         />
-        <Button fill style={{ width: '300px' }} onClick={() => navigate('add')}>
+        <Button onClick={() => navigate('add')}>
           {t('schedule.addReservation')}
         </Button>
       </Flex>
